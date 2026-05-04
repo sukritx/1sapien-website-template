ab9fd7e7640e8012e8a09ee39b53a6bf4553b7e1
sukritx <ddserial@gmail.com>
2026-05-04 12:55:09 +0700

    style(footer): remove decorative background shapes
    Remove the absolute positioned SVG shape images from the footer component to simplify the background layout.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -259,13 +259,6 @@ const Footer = () => {
       </div>
 
       <div>
-        <span className="absolute left-0 top-0 z-[-1] aspect-[95/82] w-full max-w-[570px]">
-          <Image src="/images/footer/shape-1.svg" alt="shape" fill />
-        </span>
-
-        <span className="absolute bottom-0 right-0 z-[-1] aspect-[31/22] w-full max-w-[372px]">
-          <Image src="/images/footer/shape-3.svg" alt="shape" fill />
-        </span>
 
         <span className="absolute right-0 top-0 z-[-1]">
           <svg
