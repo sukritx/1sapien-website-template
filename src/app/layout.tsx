9a3db702ea5746c3de84fe55516d07171cfcf4d5
sukritx <ddserial@gmail.com>
2026-05-18 03:00:07 +0700

    style(font): update font to Montserrat and optimize font loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,6 +87,9 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning className="!scroll-smooth" lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
